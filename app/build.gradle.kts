5f6049be8eb788a94f6a4117ee55220d5a9b3ae4
stanmoskalenko <st.moskalenko@gmail.com>
2023-11-10 21:56:50 +0300

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+    id("java")
+}
+
+group = "org.app"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("app.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

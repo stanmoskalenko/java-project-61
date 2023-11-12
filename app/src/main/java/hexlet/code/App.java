41ebbbfde36a58d9630cdcb39ac0c684d1236b1f
stanmoskalenko <st.moskalenko@gmail.com>
2023-11-12 18:50:02 +0300

    init

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,35 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
-        Cli.run();
+        var gameChoicePrev = new StringBuilder()
+                .append("Please enter the game number and press Enter.")
+                .append("\n 1 - Greet")
+                .append("\n 2 – Even")
+                .append("\n 0 - Exit")
+                .toString();
+        var sc = new Scanner(System.in);
+        System.out.println(gameChoicePrev);
+        var gameChoice = sc.nextInt();
+        System.out.println("Your choice: " + gameChoice);
+
+        switch (gameChoice) {
+            case (1):
+                var greetGame = new GreetGame();
+                greetGame.start();
+                break;
+            case (2):
+                var evenGame = new EvenGame();
+                evenGame.start();
+                break;
+            case (0):
+                Runtime.getRuntime().exit(0);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + gameChoice);
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/GreetGame.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+public class GreetGame {
+
+    public void start() {
+        System.out.println("The game doesn't exist yet! Choice another game");
+        Runtime.getRuntime().exit(0);
+    }
+}
